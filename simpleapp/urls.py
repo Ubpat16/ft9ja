d7395105e18ce8ad098a6ce8eb55abeff508f70d
ubpat16 <ubpat16@gmail.com>
2022-06-28 13:06:15 +0100

    Final Commit

--- simpleapp/urls.py
+++ simpleapp/urls.py
@@ -2,5 +2,6 @@ from simpleapp import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.view_info)
+    path('', views.view_info),
+    path('refresh/', views.refresh_data),
 ]
--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,14 +1,15 @@
-# import asyncio
-from django.http import JsonResponse
+import asyncio
+from django.http import HttpResponse, JsonResponse
+from httpx import StatusCode
 # from django.shortcuts import render
 # from rest_framework.response import Response
 from .models import DjangularDB
 from .serializers import DjangularDBSerializer
 from rest_framework.decorators import api_view
-# from .metaapi import get_data
-# import datetime as dt
-# from datetime import timedelta'
-from .task import metaapi
+from .metaapi import get_data
+import datetime as dt
+from datetime import timedelta
+
 
 # Create your views here.
 @api_view(['GET'])
@@ -18,3 +19,10 @@ def view_info(request):
     serializer = DjangularDBSerializer(view_data, many=True)
     return JsonResponse(serializer.data, safe=False)
 
+
+def refresh_data(request):
+    market_time = (dt.datetime.now() + timedelta(hours=2)).time().strftime('%H:%M:%S')
+    info = asyncio.run(get_data())
+    create_info = DjangularDB(market_watch_time=market_time, balance=info['balance'], equity=info['equity'])
+    create_info.save()
+    return HttpResponse("Success")
