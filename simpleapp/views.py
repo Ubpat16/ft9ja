2cc9306b788c2400245ef2c11662e14b7d58ec70
ubpat16 <ubpat16@gmail.com>
2022-06-29 12:51:35 +0100

    Added a build-in data refresh feature because of heroku worker time out errors

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,15 +1,8 @@
-import asyncio
-from django.http import HttpResponse, JsonResponse
-from httpx import StatusCode
-# from django.shortcuts import render
-# from rest_framework.response import Response
+from django.http import JsonResponse
 from .models import DjangularDB
 from .serializers import DjangularDBSerializer
 from rest_framework.decorators import api_view
-from .metaapi import get_data
-import datetime as dt
-from datetime import timedelta
-
+from .task import get_data
 
 # Create your views here.
 @api_view(['GET'])
@@ -18,3 +11,4 @@ def view_info(request):
     view_data = DjangularDB.objects.all()
     serializer = DjangularDBSerializer(view_data, many=True)
     return JsonResponse(serializer.data, safe=False)
+
